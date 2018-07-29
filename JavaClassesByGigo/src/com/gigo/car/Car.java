85ecaddf50a67ac270536f831af35d2ea402a398
Giridhar Gopal <g.gopal@prowareness.nl>
2018-07-29 18:02:06 +0530

    Car and Jaguar classes

--- JavaClassesByGigo/src/com/gigo/car/Car.java
+++ JavaClassesByGigo/src/com/gigo/car/Car.java
@@ -3,4 +3,16 @@ package com.gigo.car;
 public class Car {
 	String color;
 	String type;
+	
+	Car(String color, String type){
+		this.color =color;
+		this.type = type;
+	}
+	void start() {
+		System.out.println("Get Started");
+	}
+	
+	void printDescription() {
+		System.out.println("This is a "+this.color+" "+this.type);
+	}
 }
---
+++ JavaClassesByGigo/src/com/gigo/car/Jaguar.java
@@ -0,0 +1,16 @@
+package com.gigo.car;
+
+import java.awt.Point;
+
+public class Jaguar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car = new Car("Black", "JAGUAR XF");
+		car.printDescription();
+		Point point = new Point(3,4);
+		System.out.println(point);
+
+	}
+
+}
