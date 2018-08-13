ac9ebdc167e8e0cd960db47c62de089551c340f8
Giridhar Gopal <g.gopal@prowareness.nl>
2018-08-13 06:56:37 +0530

    statics and initialization blocks

--- JavaClassesByGigo/src/com/gigo/car/Car.java
+++ JavaClassesByGigo/src/com/gigo/car/Car.java
@@ -3,10 +3,31 @@ package com.gigo.car;
 public class Car {
 	String color;
 	String type;
+	int serialNumber;
+	static int carCount;
+	{
+		color ="red";
+		type = "celica";
+	}
+	Car(){
+		carCount++;
+		serialNumber = carCount;
+	}
 	
 	Car(String color, String type){
 		this.color =color;
 		this.type = type;
+	}
+	
+	String getDescription() {
+		String desc = "This is a "+color+" "+type;
+		return desc;
+	}
+	
+	void customize(String newColor, String newType) {
+		color = newColor;
+		type = newType + " " + type;
+		System.out.println(getDescription());
 	}
 	void start() {
 		System.out.println("Get Started");
@@ -14,5 +35,8 @@ public class Car {
 	
 	void printDescription() {
 		System.out.println("This is a "+this.color+" "+this.type);
+	}
+	static void resetCarCount() {
+		carCount = 0;
 	}
 }
